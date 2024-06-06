f566b3f110aa787ed60a8d360f12503f521286c2
DarinelCigarroa <darinelcigarroa97@gmail.com>
2024-06-06 07:50:07 +0000

    :children_crossing: user experience home

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,23 +1,27 @@
 <template>
   <q-page class="flex flex-center main-home row col-12">
-    <div class="content-information row col-xs-12 col-sm-12 col-md-6">
-      <div class="service-title">
-        <span style="font-size: 50px; padding: 0; margin: 0" class="text-h1">Mudanzas y fletes</span>
+    <transition appear enter-active-class="animated zoomIn slower delay-1s " leave-active-class="animated fadeOut">
+      <div class="content-information row col-xs-12 col-sm-12 col-md-6">
+        <div class="service-title">
+          <span style="font-size: 70px; padding: 0; margin: 0" class="text-h1">Mudanzas y fletes</span>
+        </div>
+        <div class="how-it-works-button">
+          <q-btn size="18px" color="primary">
+            <span class="text-white">¿Cómo funciona?</span>
+          </q-btn>
+        </div>
+        <div class="promotional-text q-my-md">
+          <span style="font-size: 25px">
+            Encuentra el Mejor Servicio de Flete y Mudanza Fácilmente.
+          </span>
+        </div>
       </div>
-      <div class="how-it-works-button">
-        <q-btn size="20px" color="primary">
-          <span class="text-white">¿Cómo funciona?</span>
-        </q-btn>
+    </transition>
+    <transition appear enter-active-class="animated zoomIn slower delay-1s " leave-active-class="animated fadeOut">
+      <div class="content-formRequest col-xs-12 col-sm-12 col-md-6 flex items-center justify-end">
+        <FormRequest></FormRequest>
       </div>
-      <div class="promotional-text q-my-md">
-        <span style="font-size: 25px">
-          Encuentra el Mejor Servicio de Flete y Mudanza Fácilmente.
-        </span>
-      </div>
-    </div>
-    <div class="content-formRequest col-xs-12 col-sm-12 col-md-6 flex items-center justify-end">
-      <FormRequest></FormRequest>
-    </div>
+    </transition>
   </q-page>
   <q-page class="home-how-works row col-12">
     <HomeHowWorks></HomeHowWorks>
@@ -53,7 +57,7 @@ defineOptions({
 
 .how-it-works-button .q-btn {
   border: 1px solid #fff;
-  transition: 0.5s;
+  transition: 0.4s;
 }
 
 .how-it-works-button .q-btn:hover {
@@ -65,11 +69,14 @@ defineOptions({
 }
 
 .content-information {
+  display: flex;
+  flex-direction: column;
   padding: 10px;
   /* background: blue !important; */
 }
 
 .content-formRequest {
+  display: flex;
   padding: 10px;
   /* background: rgb(40, 204, 122) !important; */
 }
