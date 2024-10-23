656e545fb92dddb1b792707f3e0cfba6a74670d8
darinel.cigarroa <darinel.cigarroa@infotec.mx>
2024-10-23 10:15:24 +0000

    :iphone: Resonsive desing how works

--- src/components/howWorks/HowItWorksTabs.vue
+++ src/components/howWorks/HowItWorksTabs.vue
@@ -1,21 +1,40 @@
 <template>
   <q-card class="card-detail no-shadow">
-    <q-card-section class="card-detail__content__img">
+    <div class="card-detail__img-container">
       <img :src="selectedStep.img" class="card-detail__img" />
-    </q-card-section>
+    </div>
+
+    <q-card-section class="card-detail__content__img"></q-card-section>
+
     <q-card-section class="card-detail__steps">
       <div class="card-detail__header">
         <span class="text-h6">{{ selectedStep.title }}</span>
         <span class="text-h6 text-body1">{{ selectedStep.subtitle }}</span>
       </div>
       <q-list v-for="(step, index) in selectedStep.steps" :key="index">
-        <!-- Iteramos sobre cada paso -->
-        <!-- <pre>{{ step }}</pre> -->
-        <q-item v-for="(detail, i) in step.details" :key="i">
+        <q-expansion-item
+          v-model="expanded[index]"
+          v-if="step.title"
+          expand-separator
+          icon="fa-solid fa-diamond"
+          :label="step.title"
+        >
+          <q-card>
+            <q-card-section>
+              <q-item v-for="(detail, i) in step.details" :key="i">
+                <q-item-section avatar>
+                  <q-icon color="positive" name="fa-solid fa-check" />
+                </q-item-section>
+
+                <q-item-section>{{ detail }}</q-item-section>
+              </q-item>
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+        <q-item v-for="(detail, i) in step.details" :key="i" v-else>
           <q-item-section avatar>
             <q-icon color="positive" name="fa-solid fa-check" />
           </q-item-section>
-
           <q-item-section>{{ detail }}</q-item-section>
         </q-item>
       </q-list>
@@ -24,8 +43,8 @@
 </template>
 
 <script setup>
-import { toRef } from "vue";
-// const emit = defineEmits(["closeDialog"]);
+import { toRef, ref } from "vue";
+
 const props = defineProps({
   selectedStep: {
     type: Object,
@@ -33,60 +52,111 @@ const props = defineProps({
 });
 
 const selectedStep = toRef(props.selectedStep);
-
-// const closeDialog = () => {
-//   emit("closeDialog");
-// };
+const expanded = ref([true, false]);
 </script>
+
 <style scoped>
 .card-detail {
-  background: transparent;
-  width: 80%;
-  min-width: 50%;
-  display: flex;
-  justify-content: flex-start;
-  box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1); /* Sombra suave */
-  overflow: visible; /* Permitimos que la imagen sobresalga sin ser cortada */
-  position: relative;
-}
-
-.card-detail__content__img {
+  background: var(--card);
+  width: 100%;
+  height: auto;
   display: flex;
   justify-content: center;
+  align-items: stretch;
+  flex-direction: column;
+  padding: 5px;
+  border-radius: 15px;
+}
+.card-detail__content__img {
+  display: none;
+}
+.card-detail__img-container {
+  height: auto;
+  z-index: 2;
+  display: flex;
   align-items: center;
-  background: white;
-  width: 30%; /* Ancho del contenedor de la imagen */
-  height: auto;
-  position: relative;
-  z-index: 1;
-  border-top-left-radius: 20px !important;
-  border-bottom-left-radius: 20px !important;
-  overflow: visible; /* Permitir que la imagen sobresalga */
-  left: 3px;
+  justify-content: center;
+  padding: 10px;
 }
-
 .card-detail__img {
-  width: 25em; /* Aumentamos el tamaño de la imagen */
-  height: auto;
+  width: 18em;
   object-fit: contain;
-  margin-left: -90%; /* Mover la imagen hacia la izquierda para que sobresalga sin perder el centrado */
+  z-index: 3;
 }
-
 .card-detail__header {
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
 }
-
 .card-detail__steps {
-  background: white;
-  width: 70%;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  border-top-right-radius: 20px !important; /* Borde redondeado en la esquina superior derecha */
-  border-bottom-right-radius: 20px !important; /* Borde redondeado en la esquina inferior derecha */
+  background: var(--card);
+  width: 100%;
+  height: auto;
+}
+@media (min-width: 48em) {
+  /* 768 px */
+  .card-detail {
+    display: flex;
+    flex-direction: row;
+    min-width: 100%;
+    height: auto;
+    padding: 10px;
+    background: transparent;
+    align-items: stretch;
+    position: relative;
+  }
+  .card-detail__content__img {
+    display: block;
+    width: 25%;
+    background: var(--card);
+    border-top-left-radius: 20px !important;
+    border-bottom-left-radius: 20px !important;
+    transform: translateX(55%);
+    z-index: 1;
+  }
+  .card-detail__img {
+    width: 18em;
+    object-fit: contain;
+  }
+  .card-detail__img-container {
+    position: absolute;
+    left: 0;
+    top: 50%;
+    width: 14em;
+    transform: translate(-20%, -50%);
+    top: 50%;
+    left: calc(20% - 10em);
+    transform: translateY(-50%);
+    z-index: 3;
+  }
+  .card-detail__steps {
+    flex: 1;
+    width: 75%;
+    border-radius: 15px !important;
+    z-index: 2;
+    height: auto;
+    min-height: 50vh;
+    padding-left: 3em;
+  }
+  @media (min-width: 56.25em) {
+    /* > 900 px */
+    .card-detail__img {
+      width: 20em;
+    }
+    .card-detail__content__img {
+      display: block;
+      width: 30%;
+    }
+  }
+  @media (min-width: 75em) {
+    /* > 1200 px */
+    .card-detail {
+      min-width: 70%;
+    }
+    .card-detail__img {
+      width: 28em;
+    }
+  }
 }
 </style>
