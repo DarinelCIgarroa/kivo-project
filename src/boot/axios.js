bc7fb0871d3e8c5017fc96aa92de331fd34e17cc
darinel.cigarroa <darinel.cigarroa@infotec.mx>
2024-10-24 05:44:43 +0000

    :clown_face: Error comments

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 // Configura Axios para usar la URL base desde las variables de entorno
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default boot(({ app }) => {
